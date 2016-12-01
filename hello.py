bd87917925898b5508341ff10e2f17616644d19f
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2016-12-01 02:09:50 +0100

    Add example hello.py
    * hello.py: New file.

---
+++ hello.py
@@ -0,0 +1,22 @@
+import sys
+import gi
+gi.require_version ('Gst', '1.0')
+from gi.repository import Gst
+
+Gst.init(None)
+
+playbin = Gst.ElementFactory.make("playbin", None)
+assert (playbin)
+
+playbin.set_property("uri", "file:///"+sys.argv[1])
+
+ret = playbin.set_state(Gst.State.PLAYING)
+assert (ret != Gst.StateChangeReturn.FAILURE)
+
+bus = playbin.get_bus()
+msg = bus.timed_pop_filtered (
+    Gst.CLOCK_TIME_NONE,
+    Gst.MessageType.ERROR | Gst.MessageType.EOS )
+
+playbin.set_state(Gst.State.NULL)
+
